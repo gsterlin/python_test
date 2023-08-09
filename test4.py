38f36c0905272e345b37117508d9811e6bc28230
Greg Sterling <gregsterling@Gregs-Mac-mini-3.local>
2023-08-09 12:23:14 -0400

    Add in some more explanation about how to use assert to test code and added in a fourth test that works, but behaves strangely

--- test4.py
+++ test4.py
@@ -1,13 +1,6 @@
-# An attempt to add in some checking and raise errors if there are issues
-#
-# NOTE: This is making some potentially gross assumptions about the specs
-#       for the definition of the project
-#
-# Assumptions:
-#    * the string must be at least one character long
-#    * renaming chunck_length to chunk_length is okay
-#    * chunk_length must be at least 1, splitting on zero may not make sense
-#    * fill must be a single character
+# Switching from raising exceptions to asserts... seems a better solution
+# as it will catch issues in test environments, and can be easily disabled
+# by running the command with -O or -OO to disable debug messages
 #
 # let's get some regular expressions for this
 import re
@@ -24,5 +17,6 @@ def chunkify(string, chunk_length, fill='x'):
 print(chunkify("ABCDEFG", 3, 'x'))
 print(chunkify("xyzCDQplm", 4, 'y'))
 print(chunkify("AB", 16))
+print(chunkify("", 16))
 
 pass
