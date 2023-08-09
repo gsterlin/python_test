e7001a3b1af4410eba8f0473d5241f0adf38ebc8
Greg Sterling <gregsterling@Gregs-Mac-mini-3.local>
2023-08-09 11:35:41 -0400

    add in some header comments, just to explain what the script is and note any possible unexpected changes.

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
+# This is a dummy script, it doesn't actually do anything useful
+# NOTE: the spec says chunck_length, but I changed it to chunk_length
+
 def chunkify(self, string, chunk_length, fill='x'):
     return ['ABC', 'DEF', 'Gxx']
 
